cf0fc841a6e152509742b7e93ae1340ecd9799e6
Jakob <jakob.klobcic@gmail.com>
2022-09-06 10:44:14 -0600

    validation

--- src/Home.js
+++ src/Home.js
@@ -28,30 +28,69 @@ function Home() {
   const [arrivalDate, setArrivalDate] = useState(null);
   const [email, setEmail] = useState("");
 
+  const [validateDept, setDeptValidation] = useState(false)
+  const [validateArrival, setArrivalValidation] = useState(false)
+  const [validateDeptDate, setDeptDateValidation] = useState(false)
+  const [validatearrivalDate, setArrivalDateValidation] = useState(false)
+  const [validateEmail, setEmailValidation] = useState(false)
+
 
     /*
     <AppBar position="sticky">
             <Toolbar>Stopover.Flights</Toolbar>
         </AppBar>
     */
-   const submit = async function(){
-    console.log({currentTime: new Date().toISOString(),
-      departureLocation: departure,
-      arrivalLocation: arrival,
-      departureDate: departureDate.toISOString().substring(0, 10),
-      returnDate: arrivalDate.toISOString().substring(0, 10),
-      oneWay: false,
-      email: email});
-    await API.graphql(graphqlOperation(createSearchEntry, {input: {
-      currentTime: new Date().toISOString(),
-      departureLocation: departure,
-      arrivalLocation: arrival,
-      departureDate: departureDate.toISOString().substring(0, 10),
-      returnDate: arrivalDate.toISOString().substring(0, 10),
-      oneWay: false,
-      email: email
-    }}));
-   }
+    const submit = async function(){
+      //validate
+      let errors = false;
+      if(departure==""){
+        setDeptValidation(true)
+        errors = true;
+      }
+      if(arrival==""){
+        setArrivalValidation(true)
+        errors = true;
+      }
+      if(departureDate=="" || departureDate==null){
+        setDeptDateValidation(true)
+        errors = true;
+      }
+      if(arrivalDate=="" || arrivalDate==null){
+        setArrivalDateValidation(true)
+        errors = true;
+      }
+      if(!(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email))){
+        setEmailValidation(true)
+        errors = true;
+      }
+
+      if(errors){
+        return
+      }
+      //validate
+      console.log({currentTime: new Date().toISOString(),
+        departureLocation: departure,
+        arrivalLocation: arrival,
+        departureDate: departureDate.toISOString().substring(0, 10),
+        returnDate: arrivalDate.toISOString().substring(0, 10),
+        oneWay: false,
+        email: email});
+      await API.graphql(graphqlOperation(createSearchEntry, {input: {
+        currentTime: new Date().toISOString(),
+        departureLocation: departure,
+        arrivalLocation: arrival,
+        departureDate: departureDate.toISOString().substring(0, 10),
+        returnDate: arrivalDate.toISOString().substring(0, 10),
+        oneWay: false,
+        email: email
+      }}));
+
+      setDeparture("")
+      setArrival ("")
+      setDepartureDate(null)
+      setArrivalDate(null)
+      setEmail("")
+    }
 
     const swapDestinations = function(){
       console.log(arrival)
@@ -59,9 +98,10 @@ function Home() {
       setArrival(departure)
       setDeparture(temp)
     }
+
     return (
       <div className="Home" style={{display:"flex", "flexDirection":"column"}}>
-        <Box style={{background:"#000000"}}>
+        <Box >
           <img src={TopImage} alt="Logo" style={{width:"100%", maxWidth:"100%",objectFit:'cover', height:"auto", maxHeight:"30%"}}/>
           <p style={{"position": "absolute",
             "top": "8%",
@@ -72,6 +112,7 @@ function Home() {
         </Box>
         <div style={{display:"flex", "justifyContent": "center", "paddingTop":"30px"}}>
           <TextField
+            error={validateDept}
             label="Where from?"
             variant="outlined"
             value={departure}
@@ -81,6 +122,7 @@ function Home() {
             <AutorenewIcon />
           </IconButton>
           <TextField 
+            error={validateArrival}
             label="Where to?"
             variant="outlined"
             value={arrival}
@@ -89,27 +131,30 @@ function Home() {
           />
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DesktopDatePicker
+              //error={validateDeptDate}
               label="Departure date"
               inputFormat="MM/DD/YYYY"
               value={departureDate}
               onChange={(date)=>setDepartureDate(date)}
-              renderInput={(params) => <TextField {...params} />}
+              renderInput={(params) => <TextField {...params} error={validateDeptDate}/>}
               style={{"paddingLeft":"20px"}}
             />
             <DesktopDatePicker
+              error={validatearrivalDate}
               label="Return date"
               inputFormat="MM/DD/YYYY"
               value={arrivalDate}
               onChange={(date)=>setArrivalDate(date)}
-              renderInput={(params) => <TextField {...params} />}
+              renderInput={(params) => <TextField {...params} error={validatearrivalDate}/>}
             />
         </LocalizationProvider>
         </div>
         <div style={{display:"flex", "justifyContent": "center", "paddingTop":"30px"}}>
           <TextField
+            error={validateEmail}
             label="Your email."
             variant="outlined"
-            onChange={(s)=>setEmail(s.target.value)}
+            onChange={(s)=>{setEmail(s.target.value); }}
             style={{"paddingRight":"10px", width:"400px"}}
           />
           <Button variant="contained" startIcon={<SearchIcon />} onClick={submit}>
